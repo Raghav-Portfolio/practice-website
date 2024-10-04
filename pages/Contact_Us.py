b00c6092abc97a3ae764c49716d514944af41dc7
Raghav-Git-Windows <rgs.rox@gmail.com>
2024-10-04 15:51:48 +0530

    connected email sending script to the contact page

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,21 @@
 import streamlit as st
+from send_email import send_email
+
 
 st.header('Contact Me!')
 
 with st.form(key='email_forms'):
     user_email = st.text_input('Enter your email address')
-    message =  st.text_area('Your message here please')
+    raw_message =  st.text_area('Your message here please')
+    message = f"""\
+    Subject: New email from {user_email}
+    
+    From: {user_email} 
+    {raw_message}   
+    """
     button = st.form_submit_button( )
     
     if button:
-        message = message + user_email
+        send_email(message)
+        st.info('Your email was successfully delivered')
         
--- send_email.py
+++ send_email.py
@@ -5,21 +5,19 @@ Before running the script, make sure you've set up 2 - Step Authentication from 
 and then copy the app password using password manager in Google, if you're using Google
 """
 
-host = 'smtp.gmail.com'
-port = 465
+def send_email(message):
+    host = 'smtp.gmail.com'
+    port = 465
 
-username = ''
-password = ''
-receiver = ''
-
-context = ssl.create_default_context()
-
-message = """\
-Subject: Hi!
-How are you?
-Bye
-"""
-
-with smtplib.SMTP_SSL(host, port, context= context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    username = '******'
+    password = '*****'
+    receiver = '*****'
+    context = ssl.create_default_context()
+   
+    
+    with smtplib.SMTP_SSL(host, port, context= context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    
+if __name__ == '__main__':
+    send_email()
